f82ac3ddb9137793fed11f651c853d3e9df1b70e
tensan8 <tensanf@gmail.com>
2022-09-12 11:47:20 +0800

    refactor(homepage): implemented comment model and comment counter for dashboard

--- src/app/Homepage/pages/index.tsx
+++ src/app/Homepage/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Navbar from '../../common/molecules/navbar'
 import { MapPostToCardList } from '../mappers/cardMapper'
+import { useHomepageCommentViewModel } from '../viewModels/HomepageCommentViewModel'
 import { useHomepagePostsViewModel } from '../viewModels/HomepagePostsViewModel'
 import { useHomepageUsersViewModel } from '../viewModels/HomepageUsersViewModel'
 
@@ -14,6 +15,7 @@ const Homepage = (): JSX.Element => {
 
   const { allPosts } = useHomepagePostsViewModel()
   const { allUsers } = useHomepageUsersViewModel()
+  const { allComments } = useHomepageCommentViewModel()
 
   return (
         <div>
@@ -21,8 +23,8 @@ const Homepage = (): JSX.Element => {
                 backArrowAvailable = {false}
             />
 
-            {allPosts != null && allUsers != null && allUsers.length !== 0
-              ? MapPostToCardList(allPosts, handleHomepageCardClicked, allUsers)
+            {allPosts != null && allUsers != null && allComments != null && allUsers.length !== 0
+              ? MapPostToCardList(allPosts, handleHomepageCardClicked, allUsers, allComments)
               : <div></div>
             }
         </div>
