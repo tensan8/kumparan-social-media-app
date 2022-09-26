bd2eb989012dd476708bf11ddb6af67b6abbb9d8
tensan8 <tensanf@gmail.com>
2022-09-26 19:15:54 +0800

    change app to lowercase

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import App from './app'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import App from './app'
 import './index.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
     <BrowserRouter>
-        <App/>
+        <App />
     </BrowserRouter>
 )
